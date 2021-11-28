b0d7a1dec1371d1f1596412e33b4a678db9068cf
KessyRamos <90793682+KessyRamos@users.noreply.github.com>
2021-11-28 12:21:11 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AtividadeContains
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            ContainsLetra();
+            Console.WriteLine("");
+            ContainsNome();
+        }
+        private static void ContainsLetra()
+        {
+            string nomes = "Kessy";
+            bool letra = nomes.Contains('K');
+
+            Console.WriteLine("O nome é o seguinte: Kessy");
+            Console.WriteLine($"Existe a letra K no nome?");
+            Console.Write($"Resposta: {letra}");
+        }
+        private static void ContainsNome()
+        {
+            string frase = "Ontem fui ao comercio!";
+            string palavra = "mercado";
+            bool resposta = frase.Contains(palavra);
+
+            Console.WriteLine("A frase é a seguinte: Ontem fui ao comercio! ");
+            Console.WriteLine("Existe a palavra MERCADO na frase?");
+            Console.Write( $"Resposta: {resposta}");
+        }
+        
+    }
+}
